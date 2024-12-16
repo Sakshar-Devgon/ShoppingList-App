f599cb89d98021795d80930024fdc63af30e4765
Sakshar Devgon <sakshardevgon08@gmail.com>
2024-12-16 18:58:10 +0530

    Pushing ShoppingListApp

---
+++ app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myshoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyShoppingListAppTheme {
+               ShoppingListApp()
+            }
+        }
+    }
+}
+
+
+
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun ShopAppPreview() {
+//    MyShoppingListAppTheme {
+//       ShoppingListApp()
+//    }
+//}
+
+
